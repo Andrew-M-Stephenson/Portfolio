ea21f7e3b9c3f82d9f96bd0b9b10121bff34686e
Andrew-M-Stephenson <155800091+Andrew-M-Stephenson@users.noreply.github.com>
2024-12-18 15:46:07 -0600

    attempting to add animation

---
+++ testAnimation.js
@@ -0,0 +1,4 @@
+function startAnimation() {
+    const box = document.getElementById('box'); // Select the element with id 'box'
+    box.style.animation = 'move 2s forwards'; // Trigger the animation
+  }  
