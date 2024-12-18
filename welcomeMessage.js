108c0996eaa1103945d069a19bb3aebad53e3682
Andrew-M-Stephenson <155800091+Andrew-M-Stephenson@users.noreply.github.com>
2024-12-18 15:19:28 -0600

    attempting to add a personalized welcome message

---
+++ welcomeMessage.js
@@ -0,0 +1,9 @@
+const greeting = document.querySelector('#greeting');
+const hour = new Date().getHours();
+let message = 'Hello!';
+
+if (hour < 12) message = 'Good Morning!';
+else if (hour < 18) message = 'Good Afternoon!';
+else message = 'Good Evening!';
+
+greeting.textContent = message;
